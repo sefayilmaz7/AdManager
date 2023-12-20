31f238ae00b647878631e4070f64ea36e0ffb226
Sefa <65047616+sefayilmaz7@users.noreply.github.com>
2023-12-20 22:57:46 +0300

    Initial commit

---
+++ Assets/[ADMANAGER]/Scripts/IAdManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAdManager
+{
+    void ShowInterstitial();
+    void ShowRewarded();
+    void ShowRewardedInterstitial();
+    void ShowBanner();
+    
+}
